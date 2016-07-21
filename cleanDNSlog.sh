982a70925121446be5b79729920c5566e6d04e76
Abdullahaltuhami <abdullah.tuhami@gmail.com>
2016-07-21 06:48:23 +0200

    fix bug in script

--- cleanDNSlog.sh
+++ cleanDNSlog.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#cut the 28 header lines.
-sed '1,29d' ../DNSdebugLog2
-# Convert log from Windows to Unix format to handle pesky line returns
-awk '{ sub("\r$", ""); print }' ../DNSdebugLog2 > dnsdebug.wintounix
-# Get rid of blank lines:
-sed '/^$/d' dnsdebug.wintounix > dnsdebug.nolines
 
-echo 'done converting and cleaning, Ready for python..'
+
+#cut the 28 header lines.
+sed '1,29d' DNSdebugLog2 > DNSdebugLogCut
+# Convert log from Windows to Unix format to handle pesky line returns
+awk '{ sub("\r$", ""); print }' DNSdebugLogCut > dnsdebug.wintounix
+# Get rid of blank lines:
+sed '/^$/d' dnsdebug.wintounix > DNSdebugLog2Clean
+
+echo 'Done converting'
+rm DNSdebugLogCut
+rm dnsdebug.wintounix
+echo 'Removed unnecessary files'
