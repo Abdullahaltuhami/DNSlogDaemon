b6da7acc6e3a49ce0ea0792cf9639cf846da1237
Abdullahaltuhami <abdullah.tuhami@gmail.com>
2016-07-21 06:06:40 +0200

    mod cleandDNSlog

--- cleanDNSlog.sh
+++ cleanDNSlog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #cut the 28 header lines.
-sed '1,29d' dnsdebug.txt
+sed '1,29d' ../DNSdebugLog2
 # Convert log from Windows to Unix format to handle pesky line returns
-awk '{ sub("\r$", ""); print }' dnsdebug.txt > dnsdebug.wintounix
+awk '{ sub("\r$", ""); print }' ../DNSdebugLog2 > dnsdebug.wintounix
 # Get rid of blank lines:
 sed '/^$/d' dnsdebug.wintounix > dnsdebug.nolines
 
